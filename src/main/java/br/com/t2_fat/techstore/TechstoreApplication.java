e11ccaac0fa8b7222841c9f628850817d5c65a4f
Bruno Simon  Simon tech / <contato.simon.tech@gmail.com>
2025-10-27 20:37:07 -0300

    init

---
+++ src/main/java/br/com/t2_fat/techstore/TechstoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.t2_fat.techstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/t2_fat/techstore/TechstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.t2_fat.techstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
