efe80968e299dec5ed20e73acfc78d8ecaf8e3ad
Bruno Simon  Simon tech / <contato.simon.tech@gmail.com>
2025-11-27 19:48:02 -0300

    adiciona templates 'form.html' e 'list.html' para gerenciamento de produtos

--- src/main/java/br/com/t2_fat/techstore/TechstoreApplication.java
+++ src/main/java/br/com/t2_fat/techstore/TechstoreApplication.java
@@ -2,12 +2,7 @@ package br.com.t2_fat.techstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-/* Banco de Dados
-	156.67.64.10
-	DB: u397007871_techstore
-	Usuário: u397007871_techstore
-	Senha: @luno2025FAT
-*/
+
 @SpringBootApplication
 public class TechstoreApplication {
 
