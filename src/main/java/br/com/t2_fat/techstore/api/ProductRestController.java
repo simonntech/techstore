f79edca786cf4b974657c0a2849f081001c10fec
Bruno Simon  Simon tech / <contato.simon.tech@gmail.com>
2025-11-30 09:33:21 -0300

    adiciona ProductRestController com mapeamento de API para produtos

---
+++ src/main/java/br/com/t2_fat/techstore/api/ProductRestController.java
@@ -0,0 +1,12 @@
+package br.com.t2_fat.techstore.api;
+import br.com.t2_fat.techstore.domain.Product;
+import br.com.t2_fat.techstore.repository.ProductRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductRestController {
+    
+}
