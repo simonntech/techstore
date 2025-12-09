9b06ab49c295d4487b14781a66a2ad2c7b41ca36
SimonTech <contato.simon.tech@gmail.com>
2025-12-09 15:33:12 -0300

    adiciona configuração de segurança com filtro de requisições e desabilitação de CSRF

--- src/main/java/br/com/t2_fat/techstore/security/WebSecurityConfig.java
+++ src/main/java/br/com/t2_fat/techstore/security/WebSecurityConfig.java
@@ -1,5 +1,32 @@
 package br.com.t2_fat.techstore.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class WebSecurityConfig {
+    private final CustomUserDetailsService userDetailsService;
+    private final SecurityConfig securityConfig;
+    public WebSecurityConfig(CustomUserDetailsService service,
+    SecurityConfig config){
+        this.userDetailsService = service;
+        this.securityConfig = config;
+    }
     
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http)
+    throws Exception {
+    return http
+    .csrf(csrf -> csrf.disable())
+    .authorizeHttpRequests(auth -> auth.requestMatchers("/api/products/**").hasRole("ADMIN")
+    .requestMatchers("/api/public/**").permitAll()
+    .anyRequest().authenticated()
+    )
+    .httpBasic(Customizer.withDefaults())
+    .build();
+    }
 }
