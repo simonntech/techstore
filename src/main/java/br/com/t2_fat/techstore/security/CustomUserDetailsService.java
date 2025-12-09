da3ee8d816779b6e09b9a73218d0afd97afe85a5
SimonTech <contato.simon.tech@gmail.com>
2025-12-09 15:30:42 -0300

    adiciona implementação da classe CustomUserDetailsService e configuração de segurança

--- src/main/java/br/com/t2_fat/techstore/security/CustomUserDetailsService.java
+++ src/main/java/br/com/t2_fat/techstore/security/CustomUserDetailsService.java
@@ -1,5 +1,24 @@
 package br.com.t2_fat.techstore.security;
 
-public class CustomUserDetailsService {
-    
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+ 
+    @Override
+    public User loadUserByUsername(String username)
+    throws UsernameNotFoundException {
+        if(username.equals("admin")){
+        return new User("admin", "$2a$10$7kZUl2vTI7e/...criptografado...", List.of(() -> "ROLE_ADMIN"));
+        }
+    if(username.equals("cliente")){
+ return new User("cliente", "$2a$10$7kZUl2vTI7e/...criptografado...", List.of(() -> "ROLE_CLIENTE"));
+    }
+ 
+    throw new UsernameNotFoundException("Usuário não encontrado");
+    }
 }
--- src/main/java/br/com/t2_fat/techstore/security/SecurityConfig.java
+++ src/main/java/br/com/t2_fat/techstore/security/SecurityConfig.java
@@ -1,11 +1,13 @@
 package br.com.t2_fat.techstore.security;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 @Configuration
 public class SecurityConfig {
- @Bean
- public BCryptPasswordEncoder passwordEncoder(){
- return new BCryptPasswordEncoder();
- }
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+    return new BCryptPasswordEncoder();
+    }
 }
