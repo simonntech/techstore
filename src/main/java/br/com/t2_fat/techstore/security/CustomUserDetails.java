7af311c4a76a5519880432c4b717ed3703ff6c4a
SimonTech <contato.simon.tech@gmail.com>
2025-12-09 15:27:23 -0300

    adiciona implementação da classe CustomUserDetails e configuração de segurança com BCryptPasswordEncoder

--- src/main/java/br/com/t2_fat/techstore/security/CustomUserDetails.java
+++ src/main/java/br/com/t2_fat/techstore/security/CustomUserDetails.java
@@ -1,5 +1,45 @@
 package br.com.t2_fat.techstore.security;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.GrantedAuthority;
+import java.util.Collection;
 
-public class CustomUserDetails {
-    
+public class CustomUserDetails implements UserDetails {
+    private final String username;
+    private final String password;
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    public CustomUserDetails(String username, String password,
+    Collection<? extends GrantedAuthority> authorities){
+        this.username = username;
+        this.password = password;
+        this.authorities = authorities;
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+    return authorities;
+    }
+    @Override
+    public String getPassword(){
+    return password;
+    }
+    @Override
+    public String getUsername(){
+    return username;
+    }
+    @Override
+    public boolean isAccountNonExpired(){
+    return true; // Conta nunca expira
+    }
+    @Override
+    public boolean isAccountNonLocked(){
+    return true; // Conta nunca é bloqueada
+    }
+    @Override
+    public boolean isCredentialsNonExpired(){
+    return true; // Credenciais nunca expiram
+    }
+    @Override
+    public boolean isEnabled(){
+    return true; // Conta sempre habilitada
+    }
 }
